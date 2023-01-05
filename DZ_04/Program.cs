0240fa3b183539246f0ae9cca3194cd37a4c18ed
Endnes <sergeev.krut@gmail.com>
2023-01-05 16:39:28 +0300

    Допилил задание

--- DZ_04/Program.cs
+++ DZ_04/Program.cs
@@ -7,7 +7,7 @@ int[] GetArrayOneToNumber(int number)
 
     while (index < number)
     {
-        arr[index] = GetCubeNumber(index + 1);
+        arr[index] = index + 1;
         index++;
     }
     return arr;
@@ -15,20 +15,59 @@ int[] GetArrayOneToNumber(int number)
 
 int[] GetArrayIntegerNumbers(int[] arr)
 {
-    int[] arrInteger = new int[arr.Length];
     int index = 1;
+    int[] arrInteger = new int[1];
     int count = arr.Length;
     arrInteger[0] = 1;
 
     while (index < count)
     {
-        if ((arr[index] / arr[index]) == 1)
+        if (CheckingDivisionOnPrimeNumbers(arr[index]))
         {
-            return arr; //Дописать!!!
+            Array.Resize(ref arrInteger, arrInteger.Length + 1);
+            arrInteger[arrInteger.Length - 1] = arr[index];
         }
+        index++;
+    }
+
+    return arrInteger;
+
+    bool CheckingDivisionOnPrimeNumbers(int number)
+    {
+        int index = 2;
+        while (index < number)
+        {
+            if ((number % index) == 0)
+            {
+                return false;
+            }
+            index++;
+        }
+
+        return true;
     }
 
 }
 
+void PrintArray(int[] arr)
+{
+    int index = 0;
+    int number = arr.Length;
+    while (index < number)
+    {
+        if (index == number - 1)
+        {
+            Console.Write(arr[index]);   
+        }
+        else 
+        {
+            Console.Write(arr[index] + ", ");
+        }
+        index++;
+    }
+}
+
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine()!);
+
+PrintArray(GetArrayIntegerNumbers(GetArrayOneToNumber(number)));
